890f576138444a047ee73c17472609e2f3ecc759
costefan <costefan@yandex.ru>
2016-05-07 11:30:52 +0300

    Base structure changed

---
+++ search_location/__init__.py
@@ -0,0 +1,7 @@
+from config.database import db
+
+def save_request():
+    pass
+
+def start(user_id: int, location: dict, type: int):
+    save_request()
--- server.py
+++ server.py
@@ -1,13 +1,23 @@
 import asyncio
 from aiohttp import web
-import urllib3
 
 
 class ProcessingServer:
 
     def __init__(self):
         self.app = web.Application()
+        self.app.router.add_route('GET', '/users/{id}/search_request', self.search_handler)
+        self.app.router.add_route('GET', '/users/{id}/check_request', self.check_handler)
+        web.run_app(self.app)
 
+    @asyncio.coroutine
+    def search_handler(self, request):
+        user_id = request.match_info.get('id')
+        return web.Response(text="Hi, {}".format(user_id))
+
+    @asyncio.coroutine
+    def check_handler(self, request):
+        return web.Response()
 
 if __name__ == '__main__':
     my_server = ProcessingServer()
